82ebd82f3e25a6716575343db1a96f1204b3c26a
sangheonkimme <sangheon1646@gmail.com>
2021-03-14 10:39:28 +0900

    Subscriber Auth

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -27,6 +27,9 @@ function VideoDetailPage(props) {
     }, [])
 
     if(VideoDetail.writer){
+
+        const subscribButton = VideoDetail.writer._id !== localStorage.getItem('userId') && <Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/>
+
         return (
             <Row gutter={[16, 16]} style={{ overflow: 'hidden' }}>
                 <Col lg={18} xs={24}>
@@ -35,7 +38,7 @@ function VideoDetailPage(props) {
                         <video style={{ width: '100%' }} src={`http://localhost:5000/${VideoDetail.filePath}`} controls />
             
                         <List.Item
-                            actions={[<Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/>]}
+                            actions={[ subscribButton ]}
                         >
                             <List.Item.Meta
                                 avatar={<Avatar src={VideoDetail.writer.image} />}
