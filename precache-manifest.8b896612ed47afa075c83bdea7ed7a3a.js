24173967228265cd7ca33fa337ffb60a039757ac
sangheonkimme <sangheon1646@gmail.com>
2021-03-15 13:34:35 +0900

    Updates

---
+++ precache-manifest.8b896612ed47afa075c83bdea7ed7a3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426f71ae1924e7a07ad91bb42a37b087",
+    "url": "/react_youtube_app/index.html"
+  },
+  {
+    "revision": "d710eaa82e7205773279",
+    "url": "/react_youtube_app/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "dad990db30f4e5234b49",
+    "url": "/react_youtube_app/static/css/main.18a56f17.chunk.css"
+  },
+  {
+    "revision": "d710eaa82e7205773279",
+    "url": "/react_youtube_app/static/js/2.b060a7bd.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/react_youtube_app/static/js/2.b060a7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dad990db30f4e5234b49",
+    "url": "/react_youtube_app/static/js/main.0326649a.chunk.js"
+  },
+  {
+    "revision": "31cdef386632f2327782",
+    "url": "/react_youtube_app/static/js/runtime-main.ad81def4.js"
+  }
+]);
